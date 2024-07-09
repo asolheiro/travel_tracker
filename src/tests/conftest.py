d4b32aa3e60c9aab064e293d616efe3562a5bfaa
Armando Solheiro <avgsolheiro@gmail.com>
2024-07-09 15:46:12 -0300

    feat: implementing links repository

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from src.core.models.settings.db_connection_handler import db_connection_handler
-from src.core.repositories import trips_repository, emails_invite_repository
+from src.core.repositories import trips_repository, emails_invite_repository, links_repository
 
 
 @pytest.fixture()
@@ -18,6 +18,16 @@ def email_session():
     db_connection_handler.connect_database()
     
     session = emails_invite_repository.EmailsToInviteRepository(
+        connection = db_connection_handler.get_connection()
+    )
+    
+    yield session
+
+@pytest.fixture()
+def link_session():
+    db_connection_handler.connect_database()
+    
+    session = links_repository.LinksRepository(
         connection = db_connection_handler.get_connection()
     )
     
